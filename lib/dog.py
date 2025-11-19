328585f557ae7e02c888e80c04d3d22a64c4a745
Branice Simaloi <simaloibranice@gmail.com>
2025-11-19 17:40:22 +0300

    Complete Dog class with property decorators

--- lib/dog.py
+++ lib/dog.py
@@ -1,39 +1,42 @@
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
+APPROVED_BREEDS =[
+    "Mastiff", "Chihuahua" , "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"
 ]
-
 
 class Dog:
     def __init__(self, name='Fido', breed='Mastiff'):
-        self.name = name
+        self.name = name 
         self.breed = breed
 
-    def get_name(self):
-        return self._name
-
-    def set_name(self, name):
+    @property
+    def name(self):
+        return self._name 
+    
+    @name.setter
+    def name(self, name):
         if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+            self._name = name 
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be string between 1 and 25 characters.")
+        
 
-    name = property(get_name, set_name)
-
-    def get_breed(self):
-        return self._breed
-
-    def set_breed(self, breed):
+    @property
+    def breed(self):
+        return self._breed 
+    
+    @breed.setter
+    def breed(self, breed):
         if breed in APPROVED_BREEDS:
-            self._breed = breed
+            self._breed = breed 
         else:
             raise ValueError("Breed must be in list of approved breeds.")
 
-    breed = property(get_breed, set_breed)
+
+
+
+
+
+
+
+
+ 
+     
